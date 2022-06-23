b095d9ace6ec7b4e601909fd967424a66569afa8
kusanousk <kusanousk@gmail.com>
2022-06-23 17:44:12 +0900

    コメントの作成・削除を実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,7 +1,20 @@
 class BookCommentsController < ApplicationController
   def create
+    book = Book.find(params[:book_id])
+    comment = book.book_comments.new(book_comment_params)
+    comment.user_id = current_user.id
+    comment.save
+    redirect_to book_path(book)
   end
 
   def destroy
+    BookComment.find(params[:id]).destroy
+    redirect_to book_path(book)
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
   end
 end
