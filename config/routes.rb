079a4c5e3fe9f9b7a66ba6b739cf8faac8e5e687
kusanousk <kusanousk@gmail.com>
2022-06-23 13:40:32 +0900

    FavoriteControllerを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  root :to =>"homes#top"
-  get "home/about"=>"homes#about"
+  root to: "homes#top"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
-  resources :users, only: [:index,:show,:edit,:update]
+  get "home/about" => "homes#about"
+  resources :books, only: %i[index show edit create destroy update] do
+    resource :favorites, only: %i[create destroy]
+  end
+
+  resources :users, only: %i[index show edit update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
