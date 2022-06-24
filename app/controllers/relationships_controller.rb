b5732e860649685054c2e32427da9d3154b13613
kusanousk <kusanousk@gmail.com>
2022-06-24 20:40:13 +0900

    フォローできるようにした

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,8 @@
 class RelationshipsController < ApplicationController
   def create
+    followed_id = params[:relationship][:followed_id]
+    current_user.relationships_for_followed.new(followed_id: followed_id).save
+    redirect_to user_path User.find(followed_id)
   end
 
   def destroy
