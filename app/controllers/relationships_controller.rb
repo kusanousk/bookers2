74f0fbe268a9df7a64234658c0da1ed55b1a0e88
kusanousk <kusanousk@gmail.com>
2022-06-24 21:49:33 +0900

    フォローを外せるようにした

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,4 +1,6 @@
 class RelationshipsController < ApplicationController
+  before_action :ensure_correct_user, only: [:destroy]
+
   def create
     followed_id = params[:relationship][:followed_id]
     current_user.relationships_for_followed.new(followed_id: followed_id).save
@@ -6,5 +8,17 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
+    relationship = Relationship.find(params[:id])
+    relationship.destroy
+    redirect_to user_path relationship.follow
+  end
+
+  private
+
+  def ensure_correct_user
+    relationship = Relationship.find(params[:id])
+    unless relationship.follower == current_user
+      redirect_to user_path(relationship.follow)
+    end
   end
 end
