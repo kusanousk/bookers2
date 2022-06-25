1aad0c866b418b4197c1432be41e7745386f2171
kusanousk <kusanousk@gmail.com>
2022-06-25 22:31:36 +0900

    投稿時に評価をつけられるようにした

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,7 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
+  validates :rate, presence: true, numericality: { in: 1..5 }, on: :create
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
